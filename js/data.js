628f9ac78f37245cb906cfa21e9b428dc58e814c
Maite Elizabeth Figueroa Barraza <melizafigba@gmail.com>
2022-08-22 04:09:40 -0400

    se crea un buscador para filtrar platos y se genera un modal

---
+++ js/data.js
@@ -0,0 +1,91 @@
+var foodPlace = [
+    {
+      name: "Sukine",
+      filters: "korean",
+      address: ["Santa Filomena, 2345"],
+      photo: "<img src='assets/img/korean1.png' alt='Sukine' class='img-responsive'>"
+     
+    },
+    {
+      name: "Chinese Club",
+      filters: "chinese",
+      address: ["Los trapenses, 3456"],
+      photo: "<img src='assets/img/chinese1.jpg' alt='Chinese Club' class='img-responsive' >"
+     
+    },
+    {
+      name: "Eladio",
+      filters: "barbacue",
+      address: ["Bellavista 567, Providencia"],
+      photo: "<img src='assets/img/barbecue1.jpg' alt='Eladio' class='img-responsive' >"
+      
+    },
+    {
+      name: "El Criollo",
+      filters: "chilean",
+      address: ["Agustinas 2345, Santiago Centro"],
+      photo: "<img src='assets/img/chilean1.jpg' alt='El Criollo' class='img-responsive tap'>"
+      
+    },
+    {
+      name: "El Centro",
+      filters: "chilean",
+      address: ["Huerfanos 2632, Santiago Centro"],
+      photo: "<img src='assets/img/chilean2.jpg' alt='El Centro' class='img-responsive tap'>"
+     
+    },
+    {
+      name: "Hansoban",
+      filters: "korean",
+      address: ["Santa Filomena 893, Recoleta"],
+      photo: "<img src='assets/img/korean2.jpg' alt='Hansoban' class='img-responsive tap'>"
+     
+    },
+    {
+      name: "Masita Choza",
+      filters: "chinese",
+      address:["Av. Santa Maria 449"],
+      photo: "<img src='assets/img/chinese2.jpg' alt='Masita Choza' class='img-responsive tap'>"
+     
+    },
+    {
+      name: "Parriladas Renco",
+      filters: "barbacue",
+      address: ["Huerfanos 2567, santiago Centro"],
+      photo: "<img src='assets/img/barbecue2.jpg' alt='Parrilladas Renco' class='img-responsive tap'>"
+     
+    },
+    {
+      name: "Barbacue Santi",
+      filters: "barbacue",
+      address: ["loreto 2345, santiago Centro"],
+      photo: "<img src='assets/img/barbecue3.jpg' alt='Barbacue Santi' class='img-responsive tap'>"
+     
+    },
+   
+    {
+      name: "Kim Heelsun",
+      filters: "korean",
+      address: ["Santa Filomena 993, Recoleta"],
+      photo: "<img src='assets/img/korean3.jpg' alt='Kim Heelsun' class='img-responsive tap'>"
+     
+    },
+    
+  
+    {
+      name: "Chinese Master",
+      filters: "chinese",
+      address: ["Av. Santa Maria 723"],
+      photo: "<img src='assets/img/chinese3.jpg' alt='Chinese Master' class='img-responsive tap'>"
+     
+    },
+   
+    {
+      name: "El Sureño",
+      filters: "chilean",
+      address: ["Monjitas 456, Santiago Centro"],
+      photo: "<img src='assets/img/chilean3.jpg' alt='El Sureño' class='img-responsive tap'>"
+     
+    }
+  
+  ]
--- js/main.js
+++ js/main.js
@@ -1,13 +1,47 @@
-var url = 'https://pokeapi.co/v2/pokemon/50/'
+$(document).ready(function(){
+// 	$(function(){
+//    	setTimeout(function() {
+//       $('#splash').fadeOut(1000);
+//    	}, 2000);
+//   });   // vista splash
+	
+// $('#myModal').on('shown.bs.modal', function () {
+//   $('#myInput').trigger('focus')
+// })
 
-fetch(url)
-.then(response => response.json() )
-.then(data => {
+ // $("galery").append("<a href='Akas-acapulco-sol-chair'><div class='product'><img src='img/AKAS.jpg' /><p class='producttitle'>Acapulco Sol</p><p class='productdesc>Chair</p></div></a>'");
+$("#user-filter").on("change",function(){
+//alert(this.value);
+  $("#galery").empty();
+  var prodsel=this.value;
+for(i=0 ; i< foodPlace.length; i++){
+  if(prodsel== foodPlace[i].filters)
+ $("#galery").append("<div class='col-3 modal-click box7'>"+foodPlace[i].photo+"<p class='producttitle'>"+foodPlace[i].name+"</p><p class='productdesc>Chair</p></div>'");
 
-    let element = document.getElementById('elem')
-    element.innerHTML = '<p>${data.name}</p>';
+}
 
-    console.log(data)
-})
+$(".modal-click").click(function(){ //al hacer click en el contenedor de las fotos
+  $("#myModal").modal(); //iniciando el modal desde javascript
+  var thisPhoto = $(this).children("img").attr("alt"); // guardamos la variable del Alt de la foto al hacer click
+  for(i=0 ; i< foodPlace.length; i++){ //recorremos la data
+    if (thisPhoto == foodPlace[i].name) { // si el alt de la foto es igual al nombre entonces...
+      $(".modal-title").empty(); //borramos el titulo anterior
+      $(".modal-body").empty();// borramos el contenido del modal-body (la dirección);
+      $(".modal-title").html(foodPlace[i].name); // agregamos el nuevo nombre
+      
+     for (var n = 0; n < foodPlace[i].address.length; n++) { //recorremos la data en los array address
+      $(".modal-body").append("<p class='adress-modal'>"+ foodPlace[i].address[n] +"</p>"); //agregamos la nueva dirección
+     };
 
-.catch(err => console.log(err))
+    };
+
+};
+
+
+
+});
+    
+});     
+
+
+});
